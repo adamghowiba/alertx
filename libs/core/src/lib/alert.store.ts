7b58cca7bfbc304b914bdaa07e6e6bce87c91d3f
Adam <adamware99@hotmail.com>
2023-07-03 01:12:36 +0400

    feat: promiseable alerts

--- libs/core/src/lib/alert.store.ts
+++ libs/core/src/lib/alert.store.ts
@@ -1,20 +1,48 @@
 /*
 Used to handle alert related logic
 */
-
 import { Subject, timer } from 'rxjs';
+import { nanoid } from 'nanoid';
 
-export interface Alert {
-  id: string;
+export type AlertStatus = 'success' | 'warning' | 'error' | 'info' | 'loading';
+
+export interface AlertActions {
+  name: string;
+  onClick: (alert: Alert) => void;
+}
+
+export interface Alert<Actions = any> {
+  id?: string;
   title?: string;
   message: string;
+  /**
+   * Duration to show the alert in milliseconds
+   */
   showDuration?: number;
   persist?: boolean;
+  status?: AlertStatus;
   anchorOrigin?: {
     vertical: 'top' | 'left' | 'center';
     horizontal: 'top' | 'left' | 'center';
   };
   loading?: boolean;
+  actions?: Actions | ((params: { id: string }) => Actions);
+}
+
+export interface BaseAlertResponse {
+  id: string;
+}
+
+export interface AddAlertResponse extends BaseAlertResponse {
+  placedInQueue: boolean;
+  queueSize: number;
+}
+
+export interface UpdateAlertResponse extends BaseAlertResponse {}
+export interface RemoveAlertResponse extends BaseAlertResponse {}
+
+export interface AlertPromise extends Alert {
+  alertPromise: Promise<any>;
 }
 
 export interface AlertStoreConfig {
@@ -36,41 +64,100 @@ export class AlertStore {
     return this.alerts.length >= this.maxAlerts;
   }
 
-  alert(alert: Alert) {
-    console.log(this.alerts.length, this.maxAlerts);
+  alert(alert: Alert): AddAlertResponse {
+    const id = nanoid();
+
     if (this.alerts.length >= this.maxAlerts) {
-      this.queue = [...this.queue, alert];
-      return;
+      this.queue = [...this.queue, { ...alert, id }];
+      return { id, placedInQueue: true, queueSize: this.queue.length };
     }
 
-    this.alerts = [...this.alerts, {...alert}];
+    this.alerts = [...this.alerts, { ...alert, id }];
     this.subject.next(this.alerts);
 
     if (!alert.persist) {
       timer(alert.showDuration || 3000).subscribe(() => {
-        this.remove(alert.id);
+        this.remove(id);
       });
     }
+
+    return { id, placedInQueue: false, queueSize: this.queue.length };
   }
 
-  remove(id: string) {
-    this.alerts = this.alerts.filter((alert) => alert.id !== id);
-    if (this.queue.length) {
-      const firstQueueELement = this.queue.splice(this.queue.length - 1, 1)[0];
+  async alertPromise(alert: AlertPromise): Promise<AddAlertResponse> {
+    const id = nanoid();
 
-      if (firstQueueELement) this.alert(firstQueueELement);
+    if (this.alerts.length >= this.maxAlerts) {
+      this.queue = [...this.queue, { ...alert, id }];
+      return { id, placedInQueue: true, queueSize: this.queue.length };
+    }
+
+    this.alerts = [...this.alerts, { ...alert, status: 'loading', id }];
+    this.subject.next(this.alerts);
+
+    try {
+      await alert.alertPromise;
+      this.update(id, { status: 'success' });
+    } catch (error) {
+      this.update(id, { status: 'error' });
+    }
+
+    if (!alert.persist) {
+      timer(alert.showDuration || 3000).subscribe(() => {
+        this.remove(id);
+      });
+    }
+
+    return { id, placedInQueue: false, queueSize: this.queue.length };
+  }
+
+  remove(id: string): RemoveAlertResponse | undefined {
+    const alertToRemove = this.alerts.find((alert) => alert.id === id);
+    if (!alertToRemove?.id) return;
+
+    this.alerts = this.alerts.filter((alert) => {
+      return alert.id !== id;
+    });
+
+    if (this.queue.length) {
+      const firstQueueElement = this.queue.splice(this.queue.length - 1, 1)[0];
+
+      if (firstQueueElement) this.alert(firstQueueElement);
     }
 
     this.subject.next(this.alerts);
+    return { id: alertToRemove.id };
   }
 
-  update(id: string, alerts: Alert) {
-    this.alerts = this.alerts.map((alert) => {
-      if (alert.id === id) return { ...alert, alerts };
+  update(
+    id: string,
+    alertUpdates: Partial<Alert>
+  ): UpdateAlertResponse | undefined {
+    const alertToUpdate = this.alerts.find((alert) => alert.id === id);
+    if (!alertToUpdate?.id) return;
 
-      return { ...alert };
+    let updatedAlert: Alert | undefined = undefined;
+
+    this.alerts = this.alerts.map((alert) => {
+      if (alert.id === id) {
+        updatedAlert = { ...alert, ...alertUpdates };
+        return updatedAlert;
+      }
+
+      return alert;
     });
+
     this.subject.next(this.alerts);
+    return updatedAlert;
+  }
+
+  clear() {
+    this.alerts = [];
+    this.subject.next(this.alerts);
+  }
+
+  clearQueue() {
+    this.queue = [];
   }
 
   on(callback: (alerts: Alert[]) => void) {
