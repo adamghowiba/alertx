ed75b826eafe19607c89d446058e9b9e86cc08f1
Adam Ghowiba <adamware99@hotmail.com>
2023-07-03 02:33:34 +0400

    feat: publish command in react-alertx

--- libs/react-alertx/src/index.ts
+++ libs/react-alertx/src/index.ts
@@ -5,8 +5,6 @@ import './lib/styles/alertx-tailwind.scss';
 export { AlertItem as Alert } from './lib/components/Alert/AlertItem';
 export type { AlertItemProps as AlertProps } from './lib/components/Alert/AlertItem';
 
-export { AlertItem as AlertTailwind } from './lib/components/Alert/AlertItemTailwind';
-
 export { AlertItem as AlertJsx } from './lib/components/Alert/AlertItemSJSX';
 
 export { AlertProvider } from './lib/components/AlertProvider/AlertProvider';
