beef4ad45309344661335aaf5debfeea5a2f935c
Adam Ghowiba <adamware99@hotmail.com>
2023-07-03 20:04:53 +0400

    feat: seperate type export

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,1 +1,13 @@
-export * from './lib/alert.store';
+export type {
+  AddAlertResponse,
+  Alert,
+  AlertActions,
+  AlertPromise,
+  AlertStatus,
+  AlertStoreConfig,
+  BaseAlertResponse,
+  RemoveAlertResponse,
+  UpdateAlertResponse,
+} from './lib/alert.store';
+
+export { AlertStore } from './lib/alert.store';
--- libs/react-alertx/src/index.ts
+++ libs/react-alertx/src/index.ts
@@ -1,6 +1,5 @@
 import './lib/styles/global.css';
 import './lib/styles/root.css';
-import './lib/styles/alertx-tailwind.scss';
 
 export { AlertItem as Alert } from './lib/components/Alert/AlertItem';
 export type { AlertItemProps as AlertProps } from './lib/components/Alert/AlertItem';
@@ -9,5 +8,18 @@ export { AlertItem as AlertJsx } from './lib/components/Alert/AlertItemSJSX';
 
 export { AlertProvider } from './lib/components/AlertProvider/AlertProvider';
 export type { AlertProviderProps } from './lib/components/AlertProvider/AlertProvider';
+
+export type {
+  AddAlertResponse,
+  AlertActions,
+  AlertPromise,
+  AlertStatus,
+  AlertStoreConfig,
+  BaseAlertResponse,
+  RemoveAlertResponse,
+  UpdateAlertResponse,
+} from '@alertx/core';
+
+export { AlertStore } from '@alertx/core';
 
 export { useAlerts } from './lib/hooks/use-alerts';
