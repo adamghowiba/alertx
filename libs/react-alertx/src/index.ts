404508810559671bd5f5ccb744ec80bd3304363b
Adam Ghowiba <adamware99@hotmail.com>
2023-08-04 23:50:59 +0400

    fix: export everything from alertx

--- libs/react-alertx/src/index.ts
+++ libs/react-alertx/src/index.ts
@@ -9,17 +9,5 @@ export { AlertItem as AlertJsx } from './lib/components/Alert/AlertItemSJSX';
 export { AlertProvider } from './lib/components/AlertProvider/AlertProvider';
 export type { AlertProviderProps } from './lib/components/AlertProvider/AlertProvider';
 
-export type {
-  AddAlertResponse,
-  AlertActions,
-  AlertPromise,
-  AlertStatus,
-  AlertStoreConfig,
-  BaseAlertResponse,
-  RemoveAlertResponse,
-  UpdateAlertResponse,
-} from '@alertx/core';
-
-export { AlertStore } from '@alertx/core';
-
 export { useAlerts } from './lib/hooks/use-alerts';
+export * from '@alertx/core';
